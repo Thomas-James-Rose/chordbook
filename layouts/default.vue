0422b7feaa95e87e41944c81f052d64c2d7a9c1e
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2020-11-29 14:42:23 +0000

    Delete footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,9 +36,6 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
